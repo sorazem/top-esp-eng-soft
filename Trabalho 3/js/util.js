9b938f72fe2d7b8d6bc33122a1a3ce04003b38c6
Stephanie <stephanieorazem@jovensgenios.com>
2022-07-31 20:18:34 -0300

    + trabalho final

---
+++ Trabalho 3/js/util.js
@@ -0,0 +1,13 @@
+function ajax(url, method, callback, body) {
+  let request = new XMLHttpRequest();
+  request.overrideMimeType("application/json");
+  request.open(method, url, true);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4 && request.status == "200") {
+        callback(request.responseText);
+    }
+  };
+  
+  const parsedBody = Object.entries(body).map(([key, value]) => `${key}=${value}`).join('&');
+  request.send(parsedBody);
+}
